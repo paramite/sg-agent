962a45860e1a4f6f5279ecc4fabbc384ef4397ef
Martin Magr <mmagr@redhat.com>
2021-09-13 20:03:29 +0200

    Remove the correct invalid build script
    This reverts commit f9c47d8de5d6511161e84ec926ddac665ea6f189.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+base=$(pwd)
+
+PLUGIN_DIR=${PLUGIN_DIR:-"/tmp/plugins/"}
+
+build_plugin() {
+  plugin=$1
+
+  cd "$base/$plugin"
+  echo "building $plugin"
+  go build -o "$PLUGIN_DIR/$plugin.so" -buildmode=plugin
+}
+
+build_plugin "scheduler"
